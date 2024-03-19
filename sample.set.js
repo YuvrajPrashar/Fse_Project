0121d063a05cfebb00b0d681292612213df669c2
YuvrajPrashar <anam1634.be21@chitkara.edu.in>
2024-03-19 21:28:28 +0530

    renamed sample.set to sample.set.js

--- sample.set.js
+++ sample.set.js
@@ -3,9 +3,9 @@
     "productName":"Table",
     "productPricing":2000,
     "productDescription": {
-        "color": "green,
+        "color": "green",
         "measurements": 10 * 20 * 30,
         "category": 'furniture',
         "MfdOn": '2002-12-09',
-    },
+    }
 }
